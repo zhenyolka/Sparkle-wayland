e55b49282f59750bda53121137df797958070e15
Mogryph <knight@mail.ua>
2019-07-02 02:56:31 +0300

    Workflow

--- upload/user.sh
+++ upload/user.sh
@@ -1,87 +1,108 @@
 #!/system/bin/sh
 
+#===============================================================================
+
 SPARKLE_HOME="/data/data/com.sion.sparkle/"
 CHROOT_HOME="/data/local/tmp/fedora/"
 MY_USER="sion"
 
-as_root()
+#===============================================================================
+
+x_root()
 {
-    if [ "${EUID}" != "0" ]
-    then
-        REAL_PATH=$(realpath $0)
-        su -c "/system/bin/sh ${REAL_PATH} $*"
-        exit
-    fi
+    test "$(id -u)" == "0" && return 0
+    return 1
 }
+
+run_as_root()
+{
+    su -c "/system/bin/sh $(realpath $0) $*"
+    exit
+}
+
+x_mounted()
+{
+    mountpoint -q $1 && return 0
+    return 1
+}
+
+x_running()
+{
+    test "$(pidof -s $1)" != "" && return 0
+    return 1
+}
+
+x_exists()
+{
+    test -e $1 && return 0
+    return 1
+}
+
+#===============================================================================
 
 user_install()
 {
-    as_root $*
+    x_root || run_as_root $*
     cp $0 ${SPARKLE_HOME}/user.sh
     chmod 0755 ${SPARKLE_HOME}/user.sh
+    return 0
 }
 
 user_uninstall()
 {
-    as_root $*
+    x_root || run_as_root $*
     rm ${SPARKLE_HOME}/user.sh
+    return 0
 }
+
+#===============================================================================
 
 user_start()
 {
-    echo "user start ${EUID}"
-    as_root $*
+    x_root || run_as_root $*
 
     sleep 2
 
-    mkdir -p ${CHROOT_HOME}/data
-
-    if ! mountpoint -q ${CHROOT_HOME}/data
-    then
+    x_mounted ${CHROOT_HOME}/data ||
+    (
+        mkdir -p ${CHROOT_HOME}/data
         mount -o bind /data ${CHROOT_HOME}/data
-    fi
+    )
 
-    mkdir -p ${CHROOT_HOME}/tmp
-
-    if ! mountpoint -q ${CHROOT_HOME}/tmp
-    then
+    x_mounted ${CHROOT_HOME}/tmp ||
+    (
+        mkdir -p ${CHROOT_HOME}/tmp
         mount -t tmpfs tmpfs ${CHROOT_HOME}/tmp
-    fi
+    )
 
-    if [ ! -d "${CHROOT_HOME}/tmp/sparkle" ]
-    then
+    x_exists ${CHROOT_HOME}/tmp/sparkle ||
         chroot ${CHROOT_HOME} /bin/su - ${MY_USER} -c "mkdir /tmp/sparkle"
-    fi
 
-    if [ ! -e "${CHROOT_HOME}/tmp/sparkle/wayland-0" ]
-    then
+    x_exists ${CHROOT_HOME}/tmp/sparkle/wayland-0 ||
         ln -s ${SPARKLE_HOME}/wayland-0 ${CHROOT_HOME}/tmp/sparkle/wayland-0
-    fi
 
-    XWAYLAND_PID=$(pidof -s Xwayland || true)
-    if [ "${XWAYLAND_PID}" == "" ]
-    then
+    x_running Xwayland ||
+    (
         setenforce 0
         chroot ${CHROOT_HOME} /bin/su - ${MY_USER} -c "XDG_RUNTIME_DIR=/tmp/sparkle Xwayland :0" &
         sleep 2
         #setenforce 1
         chroot ${CHROOT_HOME} /bin/su - ${MY_USER} -c "DISPLAY=:0 /bin/sh ~/.xinitrc" &
         sleep 2
-    fi
+    )
+
+    return 0
 }
 
 user_stop()
 {
-    echo "user stop ${EUID}"
-    as_root $*
+    return 0
 }
 
-set -e
-set -x
+#===============================================================================
 
-EUID=$(id -u)
+set -e -x
 
 user_$1 $*
 
-echo "Ok."
-
+#===============================================================================
