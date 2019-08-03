70b257c4f8ecb8c28903361b4fdca8ad156af7cf
Mogryph <knight@mail.ua>
2019-08-03 20:27:15 +0300

    Workflow

--- apk/app/src/main/assets/sparkle.lua
+++ apk/app/src/main/assets/sparkle.lua
@@ -1,121 +1,130 @@
-
 local sparkle = {};
 
-sparkle.shell = {};
-
-function sparkle.shell.open()
-
-    local shell = {};
+sparkle.create = function()
+    local self = {};
 
     os.execute("busybox mkfifo -m 0600 shell_stdin > /dev/null 2>&1");
     os.execute("busybox mkfifo -m 0600 shell_stdout > /dev/null 2>&1");
     os.execute("su < shell_stdin > shell_stdout &");
 
-    shell.stdin = io.open("shell_stdin", "w");
-    shell.stdout = io.open("shell_stdout", "r");
+    local stdin = io.open("shell_stdin", "w");
+    local stdout = io.open("shell_stdout", "r");
 
-    return shell;
-end
+    function self.destroy()
+        stdout.close(stdout);
+        stdin.close(stdin);
+    end
 
-function sparkle.shell.close(shell)
-    sparkle.shell.write(shell, "exit\n");
-    shell.stdout.close(shell.stdout);
-    shell.stdin.close(shell.stdin);
-end
+    local function write(text)
+        stdin.write(stdin, text);
+        stdin.flush(stdin);
+    end
 
-function sparkle.shell.write(shell, text)
-    shell.stdin.write(shell.stdin, text);
-    shell.stdin.flush(shell.stdin);
-end
-
-function sparkle.shell.read(shell)
-    local text = "";
-    while (true) do
-        local line = shell.stdout.read(shell.stdout, "*l");
-        if (string.sub(line, 1, 3) == "RC=") then
-            local rc = tonumber(string.sub(line, 4, -1));
-            return rc, text;
-        else
-            text = text .. line;
+    local function read_until_rc()
+        local text = "";
+        while (true) do
+            local line = stdout.read(stdout, "*l");
+            if (string.sub(line, 1, 3) == "RC=") then
+                local rc = tonumber(string.sub(line, 4, -1));
+                return rc, text;
+            else
+                text = text .. line;
+            end
         end
     end
-end
 
-function sparkle.shell.execute(shell, command)
-    sparkle.shell.write(shell, command .. "\n");
-    sparkle.shell.write(shell, "echo RC=$?\n");
-    local rc, text = sparkle.shell.read(shell);
-
-    return rc, text;
-end
-
-function sparkle.shell.critical(shell, command)
-    local rc, text = sparkle.shell.execute(shell, command);
-    print("critical \"" .. command .. "\" rc: " .. tostring(rc));
-    if (rc == 0) then
-        return text;
-    else
-        error();
+    local function execute(command)
+        write(command .. "\n");
+        write("echo RC=$?\n");
+        local rc, text = read_until_rc();
+        return rc, text;
     end
-end
 
-function sparkle.shell.critical_conditional(shell, command)
-    local rc, text = sparkle.shell.execute(shell, command);
-    print("critical_conditional \"" .. command .. "\" rc: " .. tostring(rc));
-    if (rc == 0) then
-        return true, text;
-    elseif (rc == 1) then
-        return false, text;
-    else
-        error();
+    function self.critical(command)
+        local rc, text = execute(command);
+        print("critical \"" .. command .. "\" rc: " .. tostring(rc));
+        if (rc == 0) then
+            return text;
+        else
+            error();
+        end
     end
-end
 
-function sparkle.shell.optional(shell, command)
-    local rc, text = sparkle.shell.execute(shell, command);
-    print("optional \"" .. command .. "\" rc: " .. tostring(rc));
-    return rc, text;
-end
-
-function sparkle.sleep(shell, time)
-    sparkle.shell.critical(shell, "busybox sleep " .. tostring(time));
-end
-
-function sparkle.is_mounted(shell, p)
-    return sparkle.shell.critical_conditional(shell, "busybox mountpoint " .. p);
-end
-
-function sparkle.exists(shell, path)
-    return sparkle.shell.critical_conditional(shell, "busybox test -e " .. path);
-end
-
-function sparkle.mount(shell, p, s, t, o)
-    local command = "busybox mount ";
-    command = command .. s;
-    if (t ~= nil) then
-        command = command .. " -t " .. t;
+    function self.critical_conditional(command)
+        local rc, text = execute(command);
+        print("critical_conditional \"" .. command .. "\" rc: " .. tostring(rc));
+        if (rc == 0) then
+            return true, text;
+        elseif (rc == 1) then
+            return false, text;
+        else
+            error();
+        end
     end
-    if (o ~= nil) then
-        command = command .. " -o " .. o;
+
+    function self.optional(command)
+        local rc, text = execute(command);
+        print("optional \"" .. command .. "\" rc: " .. tostring(rc));
+        return rc, text;
     end
-    command = command .. " " .. p;
-    sparkle.shell.critical(shell, command);
-end
 
-function sparkle.is_running(shell, name)
-    return sparkle.shell.critical_conditional(shell, "busybox pidof -s " .. name);
-end
-
-function sparkle.check_mount(shell, p, s, t, o)
-    if (not sparkle.is_mounted(shell, p)) then
-        sparkle.shell.critical(shell, "busybox mkdir -p " .. p);
-        sparkle.mount(shell, p, s, t, o);
+    function self.sleep(time)
+        self.critical("busybox sleep " .. tostring(time));
     end
+
+    function self.mounted(point)
+        return self.critical_conditional("busybox mountpoint " .. point);
+    end
+
+    function self.exists(path)
+        return self.critical_conditional("busybox test -e " .. path);
+    end
+
+    function self.running(name)
+        return self.critical_conditional("busybox pidof -s " .. name);
+    end
+
+    function self.mount(args)
+        local command = "busybox mount ";
+        command = command .. args.source;
+        if (args.type ~= nil) then
+            command = command .. " -t " .. args.type;
+        end
+        if (args.options ~= nil) then
+            command = command .. " -o " .. args.options;
+        end
+        command = command .. " " .. args.point;
+        self.critical(command);
+    end
+
+    function self.check_mount(args)
+        if (not self.mounted(args.point)) then
+            self.critical("busybox mkdir -p " .. args.point);
+            self.mount(args);
+        end
+    end
+
+    function self.chroot(args)
+        local command = "busybox chroot ";
+        if (args.path == nil) then
+            command = command .. ".";
+        else
+            command = command .. args.path;
+        end
+        command = command .. " /bin/su -";
+        if (args.user ~= nil) then
+            command = command .. " " .. args.user;
+        end
+        command = command .. " -c \"" .. args.command .. "\"";
+        self.critical(command);
+    end
+
+    return self;
 end
 
-function sparkle.chroot(shell, user, command)
-    local c = "busybox chroot . /bin/su - " .. user .. " -c \"" .. command .. "\"";
-    sparkle.shell.critical(shell, c);
-end
+
+--sparkle.static_function()
+--end
+
 
 return sparkle;
--- apk/app/src/main/assets/user.lua
+++ apk/app/src/main/assets/user.lua
@@ -1,50 +1,67 @@
+#!/usr/bin/lua
 
-package.path = package.path .. ";/data/data/com.sion.sparkle/?.lua"
-local sparkle = require "sparkle"
-
+local sparkle = require "sparkle";
 
 function start()
+    --os.execute("./user.sh");
+    start_generic_container({source="/dev/block/mmcblk0p36", point="/data/local/tmp/fedora", user="sion"});
+    --start_sparkle_container({source="/dev/block/mmcblk0p36", point="/data/local/tmp/fedora", user="sion"});
+end
 
-    -- os.execute("/data/data/com.sion.sparkle/user.sh start");
+function start_generic_container(args)
+    local s = sparkle.create();
 
-    local shell = sparkle.shell.open();
-
-    sparkle.check_mount(shell, "/data/local/tmp/fedora", "/dev/block/mmcblk0p36", nil, "noatime");
-    sparkle.shell.critical(shell, "cd /data/local/tmp/fedora");
-    if (not sparkle.exists(shell, "usr")) then
-        error();
+    if (not s.exists(args.point .. "/bin/su")) then
+        s.check_mount({source=args.source, options="noatime", point=args.point});
     end
 
-    sparkle.check_mount(shell, "dev",       "/dev",     nil,        "bind");
-    sparkle.check_mount(shell, "dev/pts",   "devpts",   "devpts",   nil);
-    sparkle.check_mount(shell, "proc",      "proc",     "proc",     nil);
-    sparkle.check_mount(shell, "sys",       "sysfs",    "sysfs",    nil);
-    sparkle.check_mount(shell, "tmp",       "tmpfs",    "tmpfs",    nil);
-    sparkle.check_mount(shell, "dev/shm",   "tmpfs",    "tmpfs",    nil);
-    sparkle.check_mount(shell, "data",      "/data",    nil,        "bind");
+    s.critical("cd " .. args.point);
 
-    sparkle.shell.optional(shell, "chcon u:object_r:app_data_file:s0 tmp");
 
-    if (not sparkle.exists(shell, "tmp/sparkle")) then
-        sparkle.chroot(shell, "sion", "mkdir /tmp/sparkle");
+    s.check_mount({source="/dev", options="bind", point="dev"});
+    s.check_mount({source="devpts", type="devpts", point="dev/pts"});
+    s.check_mount({source="proc", type="proc", point="proc"});
+    s.check_mount({source="sysfs", type="sysfs", point="sys"});
+    s.check_mount({source="tmpfs", type="tmpfs", point="tmp"});
+    s.check_mount({source="tmpfs", type="tmpfs", point="dev/shm"});
+    s.check_mount({source="/data", options="bind", point="data"});
+
+
+    s.optional("chcon u:object_r:app_data_file:s0 tmp");
+
+    if (not s.exists("tmp/sparkle")) then
+        s.chroot({user=args.user, command="mkdir /tmp/sparkle"});
     end
 
-    if (not sparkle.exists(shell, "tmp/sparkle/wayland-0")) then
-        sparkle.shell.critical(shell, "busybox ln -s /data/data/com.sion.sparkle/wayland-0 tmp/sparkle/wayland-0");
+    if (not s.exists("tmp/sparkle/wayland-0")) then
+        s.critical("busybox ln -s /data/data/com.sion.sparkle/wayland-0 tmp/sparkle/wayland-0");
     end
 
-    if (not sparkle.is_running(shell, "sshd")) then
-        sparkle.chroot(shell, "root", "/usr/sbin/sshd");
-        sparkle.sleep(shell, 1);
+    --if (not s.running("sshd")) then
+    --    s.chroot({command="/usr/sbin/sshd"});
+    --    s.sleep(1);
+    --end
+
+    if (not s.running("Xwayland")) then
+        s.chroot({user=args.user, command="XDG_RUNTIME_DIR=/tmp/sparkle Xwayland :0 -noreset &"});
+        s.sleep(1);
+        s.chroot({user=args.user, command="DISPLAY=:0 /bin/sh ~/.xinitrc &"});
+        s.sleep(1);
     end
 
-    if (not sparkle.is_running(shell, "Xwayland")) then
-        sparkle.chroot(shell, "sion", "XDG_RUNTIME_DIR=/tmp/sparkle Xwayland :0 -noreset &");
-        sparkle.sleep(shell, 1);
-        sparkle.chroot(shell, "sion", "DISPLAY=:0 /bin/sh ~/.xinitrc &");
-        sparkle.sleep(shell, 1);
+    s.destroy();
+end
+
+function start_sparkle_container(args)
+    local s = sparkle.create();
+
+    if (not s.exists(args.point .. "/bin/su")) then
+        s.check_mount({source=args.source, options="noatime", point=args.point});
     end
 
-    sparkle.shell.close(shell);
+    s.critical("cd " .. args.point);
 
+    s.critical("./init");
+
+    s.destroy();
 end
