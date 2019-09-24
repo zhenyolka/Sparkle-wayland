f5bd1901c56655824e89449779e1958a608af45a
Mogryph <knight@mail.ua>
2019-09-25 00:20:50 +0300

    Workflow

--- were/were_platform_surface_provider.cpp
+++ were/were_platform_surface_provider.cpp
@@ -1,5 +1,7 @@
 #include "were_platform_surface_provider.h"
 
+were_object_pointer<were_platform_surface_provider> were_platform_surface_provider::default_provider_;
+
 were_platform_surface_provider::~were_platform_surface_provider()
 {
 }
--- were/were_platform_surface_provider.h
+++ were/were_platform_surface_provider.h
@@ -14,6 +14,12 @@ public:
     virtual int display_width() const = 0;
     virtual int display_height() const = 0;
     virtual were_object_pointer<were_platform_surface> create_surface(int width, int height, int format) = 0;
+
+    static were_object_pointer<were_platform_surface_provider> &default_provider() { return default_provider_; }
+    static void set_default_provider(were_object_pointer<were_platform_surface_provider> provider) { default_provider_ = provider; }
+
+private:
+    static were_object_pointer<were_platform_surface_provider> default_provider_;
 };
 
 #endif // WERE_PLATFORM_SURFACE_PROVIDER_H
