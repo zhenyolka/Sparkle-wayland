417056dc56305750fb4443d80de0a2feb13ce9dd
Mogryph <knight@mail.ua>
2020-02-05 01:57:48 +0200

    user.sh improvements part 1

--- apk/app/src/main/assets/user.sh
+++ apk/app/src/main/assets/user.sh
@@ -1,24 +1,20 @@
 
-# check_mount <directory> [options] <source>
-
 source /data/data/com.sion.sparkle/files/sparkle.sh
 
 function user_start()
 {
-    #start_generic_container "/dev/block/mmcblk0p36" "/data/local/tmp/fedora" "sion"
+    #start_generic_container source="/dev/block/mmcblk0p36" point="/data/local/tmp/fedora" user="sion"
 }
 
 function start_generic_container()
 {
-    local source=$1
-    local point=$2
-    local user=$3
+    local $*
 
     check_root
 
-    exists "${point}/bin/su" || check_mount ${point} -o noatime ${source}
+    exists "${point?}/bin/su" || check_mount ${point?} -o noatime ${source?}
 
-    critical "cd ${point}"
+    critical "cd ${point?}"
 
     check_mount dev -o bind /dev
     check_mount dev/pts -t devpts devpts
@@ -32,15 +28,15 @@ function start_generic_container()
 
     exists "tmp/sparkle/wayland-0" ||
     {
-        sparkle_chroot ${user} "mkdir -p /tmp/sparkle"
+        sparkle_chroot ${user?} "mkdir -p /tmp/sparkle"
         critical "busybox ln -s /data/data/com.sion.sparkle/files/wayland-0 tmp/sparkle/wayland-0"
     }
 
     is_running "Xwayland" ||
     {
-        sparkle_chroot ${user} "XDG_RUNTIME_DIR=/tmp/sparkle Xwayland :0 -noreset &"
+        sparkle_chroot ${user?} "XDG_RUNTIME_DIR=/tmp/sparkle Xwayland :0 -noreset &"
         critical "busybox sleep 1"
-        sparkle_chroot ${user} "DISPLAY=:0 /bin/sh ~/.xinitrc &"
+        sparkle_chroot ${user?} "DISPLAY=:0 /bin/sh ~/.xinitrc &"
         critical "busybox sleep 1"
 
     }
